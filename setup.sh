12d321b975b7f4e2ae1dbefb430c56864d2d64fb
María Marino <mariamarino@Marias-MacBook-Air-2.local>
2023-11-27 13:52:45 -0300

    Adapt README.md to use case + add scipy requirement + create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clonamos el repo
+# git clone https://github.com/marinomaria/communitynotes.git
+cd communitynotes
+
+# Creamos un virtual environment
+python3.7 -m venv communitynotes
+
+# Activamos el venv
+source communitynotes/bin/activate
+
+# Instalamos las dependencias
+pip install -r requirements.txt
+
+# Descomprimimos el dataset
+mkdir -p sourcecode/data
+unzip sourcecode/data.zip -d sourcecode/data
+
+echo "Setup exitoso! :)"
